1ccae55dc90a8fe1445bded1255aa71ddf784e81
Your NamePaul <kenpopaul@yahoo.co.uk>
2022-06-06 07:22:10 +0100

    added line

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Kalen is awesome </Text>
+      <Text>Kalen is awesome ......</Text>
+      <Text>Kalen is great when he's part of a team</Text>
       <Text>I'm Blue da ba de </Text>
       <Text>da ba de dab a dee</Text>
       <StatusBar style="auto" />
