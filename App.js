fda2150c66162047dc6a3a5a15463c08aca2a32e
Your NamePaul <kenpopaul@yahoo.co.uk>
2022-06-06 07:16:10 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Kalen is awesome </Text>
+      <Text>I'm Blue da ba de </Text>
+      <Text>da ba de dab a dee</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF4',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
